a96960208cf60cdca67d9d08565732090730fadc
mardishubhradip-cloud <mardishubhradip@gmail.com>
2025-11-20 11:47:09 +0530

    new

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int add,a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    add = a + b;
+    printf("The value is %d\n",add);
+    return 0;
+}
