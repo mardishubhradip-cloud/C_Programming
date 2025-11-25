9c936bf8cefef4815403498baf45821fdc8cd9f9
mardishubhradip-cloud <mardishubhradip@gmail.com>
2025-11-25 13:45:52 +0530

    small tweaks

--- merge.c
+++ merge.c
@@ -1,46 +1,40 @@
 #include <stdio.h>
 
-void merge(int a[], int m, int b[], int n, int c[]) {
-    int i = 0, j = 0, k = 0;
-    while (i < m || j < n) {
-        if (a[i] < b[j]) {
-            c[k++] = a[i++];
-        } else {
-            c[k++] = b[j++];
-        }
-    }
-    while (i < m) {
-        c[k++] = a[i++];
-    }
-    while (j < n) {
-        c[k++] = b[j++];
-    }
-}
-
 int main() {
     int n1, n2;
-    printf("Enter the size of the first array: ");
+
+    printf("Enter size of first array: ");
     scanf("%d", &n1);
-    int a[n1];
-    printf("Enter the elements of the first sorted array:\n");
-    for(int i = 0; i < n1; i++) {
-        scanf("%d", &a[i]);
-    }
-    printf("Enter the size of the second array: ");
+
+    int A[n1];
+    printf("Enter %d sorted elements: ", n1);
+    for (int i = 0; i < n1; i++)
+        scanf("%d", &A[i]);
+
+    printf("Enter size of second array: ");
     scanf("%d", &n2);
-    int arr2[n2];
-    printf("Enter the elements of the second sorted array:\n");
-    for(int i = 0; i < n2; i++) {
-        scanf("%d", &arr2[i]);
+
+    int B[n2];
+    printf("Enter %d sorted elements: ", n2);
+    for (int i = 0; i < n2; i++)
+        scanf("%d", &B[i]);
+
+    int C[n1 + n2];
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1 && j < n2) {
+        if (A[i] < B[j])
+            C[k++] = A[i++];
+        else
+            C[k++] = B[j++];
     }
 
-    int merged[n1 + n2];
-    merge(a, n1, arr2, n2, merged);
+ while (i < n1) C[k++] = A[i++];
+    while (j < n2) C[k++] = B[j++];
 
-    printf("Merged sorted array: ");
-    for(int i = 0; i < n1 + n2; i++) {
-        printf("%d ", merged[i]);
-    }
-    printf("\n");
-    return 0;
+    printf("Merged Sorted Array:\n");
+    for (int x = 0; x < n1 + n2; x++)
+        printf("%d ", C[x]);
+
+return 0;
 }
